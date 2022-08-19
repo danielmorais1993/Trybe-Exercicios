5cbf2a0d5bacfc3618e7a6d20fba3c6cb4fc70b1
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-19 10:59:10 -0300

    enviando exercicios numero 2.

---
+++ fundamentos/4javascript/01-javascript-primeiros-passos/Exercicio-02/script.js
@@ -0,0 +1,26 @@
+let n1 ;
+
+let n2 ;
+
+
+
+
+
+if (n1 > n2)
+
+console.log(n1 + " é  maior que "+ n2);
+
+else if ( n1 === n2)
+
+console.log(n1 + " é igual a " + n2);
+
+else 
+console.log(n1 + " é menor que " + n2);
+
+
+
+
+
+
+
+
