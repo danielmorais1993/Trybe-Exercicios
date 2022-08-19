7c9fc338ab125b26c2a4f5c988ffefbaa218e233
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-19 16:09:15 -0300

    enviando exercicios da acdemia de logica 03.

--- fundamentos/4javascript/01-javascript-primeiros-passos/Exercicio-conteúdo-05/script.js
+++ fundamentos/4javascript/01-javascript-primeiros-passos/Exercicio-conteúdo-05/script.js
@@ -1,3 +1,3 @@
-weekDay="quarta-feira";
+    weekDay="quarta-feira";
 if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira" );
 console.log(weekDay);
