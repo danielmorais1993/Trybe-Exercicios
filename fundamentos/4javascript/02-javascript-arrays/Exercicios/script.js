161600e92dc5f87195f6c8a876d1d4ff3b7543b5
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-22 18:23:29 -0300

    enviando exercicios do dia 22 4.3.

--- fundamentos/4javascript/02-javascript-arrays/Exercicios/script.js
+++ fundamentos/4javascript/02-javascript-arrays/Exercicios/script.js
@@ -191,11 +191,11 @@
 // if (numerosEncontrados.length === 0){
 //     console.log("Não enontrado");
 // }
-const n = 9;
+// const n = 9;
 
-let resultado = 100;
-for (let index = 0; index <= n; index += 1) {
-  resultado -= index;
-};
-console.log(resultado);
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
 
