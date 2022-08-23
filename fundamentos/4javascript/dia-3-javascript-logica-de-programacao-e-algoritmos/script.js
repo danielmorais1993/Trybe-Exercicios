266cabcc2fce043a5fbc6bec16a091f3d96cec1c
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-23 15:00:37 -0300

    Exercício 3

--- fundamentos/4javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/4javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,61 +1,75 @@
 
-//Primeiro devemos declarar uma variável Numero que desejamos aplicar a fatoração.
-// let Numero= 10;
+// //Primeiro devemos declarar uma variável Numero que desejamos aplicar a fatoração.
+// // let Numero= 10;
 
-// //Segundo , deveremos aplicar uma estrutura de repetiçào n tal estrutura deverá ser feita de modo reverso , ouseja , começando do final e depois regredindo 
-// let a=1;
-// for (i=Numero;i>0;i--){
+// // //Segundo , deveremos aplicar uma estrutura de repetiçào n tal estrutura deverá ser feita de modo reverso , ouseja , começando do final e depois regredindo 
+// // let a=1;
+// // for (i=Numero;i>0;i--){
 
-//     a=a*i
+// //     a=a*i
 
 
      
   
     
 
-// }
-// // agora devemos aplicar o conlsole.log
-// console.log(a);
+// // }
+// // // agora devemos aplicar o conlsole.log
+// // console.log(a);
 
 
-// let word= "Marcela" ;
-// Resultado=" ";
-// for (i=word.length-1;i>=0;i--){
-//     Resultado = Resultado+word[i];
-// }
-// console.log(Resultado);
+// // let word= "Marcela" ;
+// // Resultado=" ";
+// // for (i=word.length-1;i>=0;i--){
+// //     Resultado = Resultado+word[i];
+// // }
+// // console.log(Resultado);
 
 
-IdadeNoel = 50;
-IdadeEllen = 30;
-IdadeNasc  =35;
+// // IdadeNoel = 50;
+// // IdadeEllen = 30;
+// // IdadeNasc  =35;
 
 
-    if (IdadeNoel>=IdadeEllen && IdadeEllen>IdadeNasc){
-        console.log("Nasc é a pessoa mais nova");
+// //     if (IdadeNoel>=IdadeEllen && IdadeEllen>IdadeNasc){
+// //         console.log("Nasc é a pessoa mais nova");
 
     
 
-    }else if(IdadeNoel<IdadeEllen && IdadeEllen<=IdadeNasc){
-        console.log("Noel é a pessoa mais nova")
+// //     }else if(IdadeNoel<IdadeEllen && IdadeEllen<=IdadeNasc){
+// //         console.log("Noel é a pessoa mais nova")
         
-    }else if(IdadeNoel===IdadeEllen && IdadeEllen < IdadeNasc){
-        console.log("Noel e Ellen sao as pessoas mais novas");
+// //     }else if(IdadeNoel===IdadeEllen && IdadeEllen < IdadeNasc){
+// //         console.log("Noel e Ellen sao as pessoas mais novas");
 
-    }else if (IdadeNoel>=IdadeNasc && IdadeNasc>IdadeEllen){
-        console.log("Ellen é a pessoa mais nova");
-    }else if (IdadeNoel===IdadeNasc && IdadeNasc< IdadeEllen){
-        console.log("Noel e Nasc sao as pessoas mais novas");
+// //     }else if (IdadeNoel>=IdadeNasc && IdadeNasc>IdadeEllen){
+// //         console.log("Ellen é a pessoa mais nova");
+// //     }else if (IdadeNoel===IdadeNasc && IdadeNasc< IdadeEllen){
+// //         console.log("Noel e Nasc sao as pessoas mais novas");
+// //     }else if (IdadeNoel>IdadeNasc && IdadeNasc===IdadeEllen){
+// //         console.log("Ellen e Nasc sao os mais novos");
+// //     }else if(IdadeNoel===IdadeNasc && IdadeNasc===IdadeEllen){
+// //         console.log("Noel ,Nasc e Ellen tem idade iguais");
+// //     }
+// //     else{
+// //         console.log("idade inválida");
+// //     }
 
-    }else if (IdadeNoel>IdadeNasc && IdadeNasc===IdadeEllen){
-        console.log("Ellen e Nasc sao os mais novos");
-    }else if(IdadeNoel===IdadeNasc && IdadeNasc===IdadeEllen){
-        console.log("Noel ,Nasc e Ellen tem idade iguais");
-    }
-    else{
-        console.log("idade inválida");
-    }
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
 
+
+// for (i=0;i<array.length;i++){
+//   let NumerosMenores = [];
+//   for (j=0;j<array.length;j++){
+//     if (array[i].length > array[j].length)
+//     NumerosMenores.push(array[j]);
+    
+//   }
+  
+// if (NumerosMenores.length === 4){
+// console.log (array[i]);
+//   }
+// }
 
     
      
