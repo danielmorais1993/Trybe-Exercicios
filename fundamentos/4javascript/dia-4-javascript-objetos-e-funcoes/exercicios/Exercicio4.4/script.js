cf74ac45824958d5cdbbb2a481edf6c6d20013a6
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-23 23:18:09 -0300

    Exercícios conteúdo  4.4

---
+++ fundamentos/4javascript/dia-4-javascript-objetos-e-funcoes/exercicios/Exercicio4.4/script.js
@@ -0,0 +1,62 @@
+//Exercicio 1 parte I objetos
+
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// console.log("Bem-vinda ",info['personagem']);
+
+//Exercicio 2
+info['recorrente']='sim';
+
+// console.log(info);
+
+
+//Exercicio 3
+// for (key in info){
+//   console.log(key);
+// }
+// for (key in info){
+//   console.log(info[key]);
+// }
+
+// info['personagem'] = {'Tio Patinhas'} 
+// info['origem'] = {"Christmas on Bear Mountain, Dell's Four Color Comics #178"};
+// info['nota'] ={'O último MacPatinhas'};
+// info['recorrente']=info['recorrente'] - 'sim';
+// info['recorrente']='ambas';
+//  console.log(info);
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+// console.log('O livro favorito de ' + leitor['nome'] + " " + leitor['sobrenome'] + ' se chama ' + leitor['livrosFavoritos']['0']['titulo']);
+let objeto={
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+
+}
+leitor['livrosFavoritos'].push(objeto);
+
+// console.log(leitor);
+
+
+console.log(leitor['nome'] + ' ' + 'tem '+ leitor['livrosFavoritos'].length + ' ' + "Livros favoritos"  )
+
+
+
+
