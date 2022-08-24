d17e93984c7d4c8d04fd04fa675e0802676d0081
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-23 22:06:48 -0300

    Exercícios conteúdo  4.4

---
+++ fundamentos/4javascript/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,37 @@
+
+
+// //exercicio de conteudo 
+// let player = {
+
+//  name:'Marta',
+//  lastName: 'Silva',
+//  age: 34,
+//  medals: { golden: 2, silver: 3 }
+// }
+
+
+
+// player['bestInTheWorld']=[2006, 2007, 2008, 2009, 2010, 2018];
+
+// console.log("A Jogadora " + player['name'] + " " + player['lastName'] + " foi a melhor do mundo " + player['bestInTheWorld'].length + " vezes ");
+
+// console.log("A Jogadora " + player['name'] + " " + player['lastName'] + " possui " + player['medals']['golden'] + " medalhas de ouro" + " e " + player['medals']['silver'] + " de prata "  )
+
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge',
+// };
+// for (let i in names){
+//   console.log("olá " + names[i]);
+// };
+
+// let car = {
+//   model: 'A3 Sedan',
+//   manufacturer: 'Audi',
+//   year: 2020
+// }
+// for (let [key, value] of (car)) {
+//   console.log(key + ' ' + value);
+// }
+
