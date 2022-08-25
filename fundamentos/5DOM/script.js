57b194467a50e2cc6a89e14445cb1c29657340d6
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-08-25 19:19:24 -0300

    Exercicio e conteudo dia 5.1

---
+++ fundamentos/5DOM/script.js
@@ -0,0 +1,17 @@
+// console.log(document.getElementById("test"));
+// //recebe como parametro um Id 
+// document.getElementById("test").innerHTML = "Olá meu querido";
+
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+// Por fim, recupere o subtítulo e altere-o também.
+
+// document.getElementById("page-title").innerText="Beleza Americana";
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.color="magenta";
+// const subtitle=document.getElementById("subtitle");
+// subtitle.style.background="blue";
+document.getElementsByClassName("paragraph")[0].style.background="blue";
+document.getElementsByTagName("h4")[0].style.color="lightblue";
+
+
