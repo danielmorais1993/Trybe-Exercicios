d208ba74940483be00fdaa5af1dd8babd632eddb
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-09-08 13:10:46 -0300

    ready

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/Exercicios6.1/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/Exercicios6.1/script.js
@@ -1,24 +1,37 @@
 const buttonSend = document.getElementById('btn-send');
-const buttonErase= document.getElementById('btn-clear');
+const buttonErase = document.getElementById('btn-clear');
+const nome = document.querySelector('#name')
+const email = document.querySelector('#email')
+const textarea = document.querySelector('#text-area')
 
 
 
-function callButton(event){
+
+function checkAvailability(evento, Min, Max) {
+  if (evento.value.length < Min || evento.value.length > Max) {
+    window.alert('Invalid Data');
+   
+     
+
+}
+}
+
+
+
+
+function callButton(event) {
+
   event.preventDefault();
+  console.log(event.target)
+  checkAvailability(nome, 10, 40);
+  checkAvailability(email, 10, 50);
+  checkAvailability(textarea, 0, 500);
+  
 
-}
 
-function Erase(event){
-  document.querySelector('#name').value='';
-  document.querySelector('#email').value='';
-  document.querySelector('#opt1').checked=false;
-  document.querySelector('#opt2').checked=false;
-  document.querySelector('#opt3').checked=false;
-  document.querySelector('#opt4').checked=false;
-  document.querySelector('#date-call').value='';
-  document.querySelector('#check1').checked=false;
-  document.querySelector('#check2').checked=false;
-  document.querySelector('#text-area').value='';
+
+  return ('All data was sent')
+
 
 
 
@@ -26,10 +39,29 @@ function Erase(event){
 
 }
 
+function Erase(event) {
+  document.querySelector('#name').value = '';
+  document.querySelector('#email').value = '';
+  document.querySelector('#opt1').checked = false;
+  document.querySelector('#opt2').checked = false;
+  document.querySelector('#opt3').checked = false;
+  document.querySelector('#opt4').checked = false;
+  document.querySelector('#date-call').value = '';
+  document.querySelector('#check1').checked = false;
+  document.querySelector('#check2').checked = false;
+  document.querySelector('#text-area').value = '';
 
 
 
-buttonSend.addEventListener('click',callButton)
+
+
+}
+
+
+
+
+buttonSend.addEventListener('click', callButton)
+
+
 
 buttonErase.addEventListener('click', Erase)
-
---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,29 @@
+// SELETORES
+// const INPUT_TEXT = document.querySelector("#input-text");
+// const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+// const HREF_LINK = document.querySelector("#href");
+
+// function Botao(event){
+//   event.preventDefault()
+// }
+// function Botao2(event){
+//   event.preventDefault();
+// }
+// function Botao3(event){
+//   event.preventDefault();
+// }
+
+
+
+// HREF_LINK.addEventListener('click',Botao)
+
+
+// INPUT_CHECKBOX.addEventListener('click',Botao2)
+
+// INPUT_TEXT ('keypress',Botao3)
+
+
+
+
+
+
