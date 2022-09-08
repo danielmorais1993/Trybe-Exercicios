f10cafb1a30e01d30fcd2d4fd0b37e98c2f1c5e8
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-09-08 15:56:49 -0300

    ready

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/academia-de-lógica/script.js
@@ -0,0 +1,8 @@
+const checkIn =document.querySelector('.inputCheckin');
+const checkOut= document.querySelector('.inputCheckOut');
+const submitBtn= document.querySelector('#submit-btn');
+
+submitBtn.addEventListener('click',() => {
+  event.preventDefaul
+
+}
