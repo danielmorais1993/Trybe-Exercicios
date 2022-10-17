e8c46bbaaa276a4c86de3a2ab2d81a67f07190e6
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-10-17 19:42:28 -0300

    Studyng React

---
+++ FrontEnd/Secao-02-Introducao-React/dia-01-Hello-World-no-react/conteudo/my_app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css';
+import Mago from './mago'
+
+function App(){
+  return < Mago/>
+}
+
+
+export default App;
---
+++ FrontEnd/Secao-02-Introducao-React/dia-01-Hello-World-no-react/conteudo/my_app/src/mago.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+class Mago extends Component{
+  render(){
+    return <span>Hello World!</span>
+  }
+}
+export default Mago
